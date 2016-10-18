e4c661b79b45167ace4c336cfc69664afc26da73
Kris Hermans <kris.hermans@live.be>
2016-10-18 22:10:10 +0200

    Got Test Infrastructure going
    setup xUnit
    setup Test Runner on Android

---
+++ CallTheBus.Tests/BuzzAlerterTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using CallTheBus.ViewModel;
+
+namespace CallTheBus.Tests
+{
+    public class BuzzAlerterTests
+    {
+        [Fact]
+        public void AlertMessage_WhenCreated_ReturnsStop()
+        {
+            var sut = new BuzzAlerterViewModel();
+            Assert.Equal("Stop!", sut.AlertMessage);
+        }
+    }
+}
---
+++ CallTheBus/CallTheBus/ViewModel/BuzzAlerterViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallTheBus.ViewModel
+{
+    public class BuzzAlerterViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _alertMessage;
+
+        public string AlertMessage
+        {
+            get { return _alertMessage; }
+            set
+            {
+                _alertMessage = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(AlertMessage)));
+            }
+        }
+
+    }
+}
